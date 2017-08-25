77e03e3b3ed70019ea9c2bceb41f5097f11dd1e1
Lourens de Villiers (Noki) <lpdevilliers@gmail.com>
2017-08-25 07:18:46 -0400

    Updated functionality

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-
-import { GameContainer } from "./components/GameContainer";
-
-ReactDOM.render(
-    <GameContainer />,
-    document.getElementById("app")
-);
